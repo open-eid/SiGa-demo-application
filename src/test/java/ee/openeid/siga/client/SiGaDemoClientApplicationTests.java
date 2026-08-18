a6b60e7044f088942f09f9da3bad17516ea33341
Ivo Mattus <ivo.mattus@nortal.com>
2026-08-18 15:21:05 +0300

    SIGA-1339 Remove docker-compose parameters from configuration and delete bundled TLS keystore/truststore

--- src/test/java/ee/openeid/siga/client/SiGaDemoClientApplicationTests.java
+++ src/test/java/ee/openeid/siga/client/SiGaDemoClientApplicationTests.java
@@ -2,10 +2,12 @@ package ee.openeid.siga.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@TestPropertySource(properties = "siga.api.trust-store=classpath:empty.p12")
 class SiGaDemoClientApplicationTests {
 
     @Test
